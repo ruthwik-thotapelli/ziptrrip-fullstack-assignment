67099d5d437dfcf9f5f6466a6f410b1165be90ea
ruthwik <ruthwikreddyladdu@gmail.com>
2026-06-25 23:26:05 +0530

    Add frontend/src/main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+/**
+ * main.jsx
+ * React application entry point.
+ * Wraps the app with BrowserRouter to enable React Router navigation.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
